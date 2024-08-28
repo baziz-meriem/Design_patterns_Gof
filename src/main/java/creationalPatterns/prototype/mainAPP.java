a83b3b5f12bd95a74e44f69c63734b0991b568ee
baziz-meriem <jm_baziz@esi.dz>
2024-08-28 10:09:07 +0100

    prototype

--- src/main/java/creationalPatterns/prototype/mainAPP.java
+++ src/main/java/creationalPatterns/prototype/mainAPP.java
@@ -2,6 +2,13 @@ package creationalPatterns.prototype;
 
 public class mainAPP {
     public static void main(String args[]){
+        LetterDoc letter = new LetterDoc();
+        letter.setTitle("letter title");
+        letter.setContent("letter content");
+        letter.setDestination("Company NAME");
+        letter.setSender("Meriem Baziz");
 
+        LetterDoc copy = letter.clone();
+        System.out.println(copy.toString());
     }
 }
